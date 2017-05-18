123e9135e052709b99614d582b6c16c80f568d1b
farooqmajeed <farooq306@hotmail.com>
2017-05-18 17:28:26 +0500

    download & search

--- src/containers/Navigation/Navigation.jsx
+++ src/containers/Navigation/Navigation.jsx
@@ -60,7 +60,7 @@ class Navigation extends Component {
   render() {
     return (
       <div className="navigation-container">
-        <MUI.AppBar style={this.props.styles} title="React Boilerplate"
+        <MUI.AppBar style={this.props.styles} title="Bulk Audio downloader"
           onLeftIconButtonTouchTap={this.props.drawerToggle}
           />
         <MUI.Drawer open={this.props.drawerOpen} docked={false}
