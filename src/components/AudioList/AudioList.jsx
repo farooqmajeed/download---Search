428ca718b9d59353a1313fa7fb3725680c51ec67
farooqmajeed <farooq306@hotmail.com>
2017-05-24 16:35:43 +0500

    download & search

--- src/components/AudioList/AudioList.jsx
+++ src/components/AudioList/AudioList.jsx
@@ -1,59 +1,79 @@
 import React, { Component } from 'react';
-import * as MUI from 'material-ui'
+import * as MUI from 'material-ui';
+import { Observable } from "rxjs";
 import styles from './AudioListStyle.js'
 import SvgIconSearch from 'material-ui/svg-icons/action/search'
 
 class AudioList extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
             progressbar: false,
             valid: '',
-            inputValidation: true
-
+            inputValidation: true,
+            errorMessage: ''
         }
         this.searchFiles = this.searchFiles.bind(this);
         this.handleChange = this.handleChange.bind(this);
+
     }
 
     downloadFiles = () => {
         if (this.refs && this.refs.search && this.refs.search.getValue()) {
-            var url = " http://127.0.0.1:5000/downloadFile?filenameList=" + this.refs.search.getValue();
+            var url = " http://127.0.0.1:5000/downloadFile?filename=" + this.refs.search.getValue();
             window.location.href = url;
+            this.setState({
+                errorMessage: ''
+            })
+        } else {
+            this.setState({
+                errorMessage: "Error Occured !! Please Insert file name and then Download"
+
+            })
         }
+
+        /*
+        if (this.props && this.props.audioList && this.props.audioList.length) {
+            this.props.fetchDownloadData(this.props.audioList);
+            console.log("download files", this.props.audioList);
+        }
+        */
     }
 
 
     searchFiles(e) {
-        this.setState({
-            progressbar: true
-        })
         if (this.refs && this.refs.search && this.refs.search.getValue()) {
             this.props.fetchAudioData(this.refs.search.getValue());
-
+            console.log("data");
+            //this.setState({ progressbar: true })
         }
+
     }
-
-    handleChange(e) {
-
+    handleChange(event) {
+        var that = this;
         this.setState({
-            valid: e.target.value
+            valid: event.target.value
         })
-        if (this.state.valid.length == 9) {
-            this.state.inputValidation = false;
-        }
+        setTimeout(() => {
+            if (that.state.valid.length == 10) {
+                that.setState({
+                    inputValidation: false,
+                     errorMessage: ''
 
-
-
+                })
+            } else {
+                that.setState({
+                    inputValidation: true,
+                    errorMessage: ''
+                })
+            }
+        }, 20)
     }
 
 
     render() {
-        var searchInputLength = true
-        let download = this.props.file;
-        console.log("download", download);
         let fileName = this.props.audioList;
+        // console.log("audiolist", fileName)
         let allFiles = [];
         if (fileName && fileName.length) {
             var innerData = JSON.parse(fileName);
@@ -63,9 +83,10 @@ class AudioList extends Component {
             });
 
         }
+
         return (
             <div >
-                <MUI.Card style={styles.muiListContainer} >
+                <MUI.Card style={styles.muiListContainer}>
                     <MUI.AppBar showMenuIconButton={false} style={styles.appbarContainer} >
                         <MUI.Avatar icon={<SvgIconSearch />} style={styles.searchBar} />
                         <MUI.TextField
@@ -73,18 +94,17 @@ class AudioList extends Component {
                             hintText="Test searching"
                             fullWidth={true}
                             style={styles.textFeild}
-                            value={this.state.valid}
-                            onChange={this.handleChange}
+                            onKeyUp={this.handleChange}
                             />
 
                         <div>
-                            <MUI.RaisedButton label="Search"   style={styles.searchbtn} primary={true} disabled={this.state.inputValidation} onTouchTap={this.searchFiles} />
+
+                            <MUI.RaisedButton label="Search" primary={true} style={styles.searchbtn} disabled={this.state.inputValidation} onTouchTap={this.searchFiles} />
                         </div>
                     </MUI.AppBar>
                     <br />
-
                     <div style={styles.buttonConainer}>
-                        <MUI.RaisedButton label="Download" primary={true} onTouchTap={this.downloadFiles} />
+                        <MUI.RaisedButton label="Download" primary={true}   onTouchTap={this.downloadFiles.bind(this)} />
                     </div>
 
 
@@ -93,23 +113,22 @@ class AudioList extends Component {
                     <div style={styles.wrapper} >
                         <h4 style={styles.heading}> Enter number in search bar to view list  </h4>
 
+                        <h4 style={styles.error}> {this.state.errorMessage} </h4>
+                      
                         {allFiles && allFiles.length ? allFiles.map((data, index) => {
                             return (<MUI.Chip
                                 key={index}
                                 style={styles.chip}
-                                onTouchTap={this.handleTouchTap}
+                                onchange={this.state.progressbar == false}
+
                                 >
                                 {data}
                             </MUI.Chip>)
                         }) : false}
 
                     </div>
-                    {this.state.progressbar == true ? <MUI.CircularProgress style = {styles.progress} size={80}>
-                        autoHideDuration ={3000}
-                    </MUI.CircularProgress> : false}
+
                 </MUI.Card>
-
-
                 <br />
 
             </div>
